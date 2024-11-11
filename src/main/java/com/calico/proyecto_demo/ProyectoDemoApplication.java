9a83cbb000d3731cd146a4a020c3d72fd9d515c0
TakUHG <sofiavasqueztabares@gmail.com>
2024-11-10 23:48:05 -0500

    Primer commit del proyecto

---
+++ src/main/java/com/calico/proyecto_demo/ProyectoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.calico.proyecto_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/calico/proyecto_demo/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.calico.proyecto_demo.repository;
+
+import com.calico.proyecto_demo.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+}
